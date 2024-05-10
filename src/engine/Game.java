82b08fb3d21be6a84b2413af0edfd96eb9095ccc
WillN-Git <wfndefo@gmail.com>
2024-05-10 03:09:56 +0200

    Add : mesh in rendering engine

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -2,9 +2,23 @@ package engine;
 
 import org.lwjgl.input.Keyboard;
 
+import engine.math.Vector3f;
+import engine.rendering.Mesh;
+import engine.rendering.Vertex;
+
 public class Game {
+	private Mesh mesh;
+	
 	public Game() {
+		mesh = new Mesh();
 		
+		Vertex[] data = new Vertex[] {
+			new Vertex(new Vector3f(-1, -1, 0)),
+			new Vertex(new Vector3f(0, 1, 0)),
+			new Vertex(new Vector3f(1, -1, 0)),
+		};
+		
+		mesh.addVertices(data);
 	}
 	
 	public void pollInput() {
@@ -28,6 +42,6 @@ public class Game {
 	}
 	
 	public void render() {
-		
+		mesh.draw();
 	}
 }
--- src/engine/rendering/RenderUtil.java
+++ src/engine/rendering/RenderUtil.java
@@ -3,8 +3,12 @@ package engine.rendering;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL30.*;
 
+import java.nio.FloatBuffer;
 
-public class RenderUtil {
+import org.lwjgl.BufferUtils;
+
+
+public class RenderUtil {	
 	public static void clearScreen() {
 		// TODO : Stencil buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -22,5 +26,23 @@ public class RenderUtil {
 		// TODO : Depth clamp for later
 		
 		glEnable(GL_FRAMEBUFFER_SRGB); // gamma correction
+	}
+	
+	public static FloatBuffer createFloatBuffer(int size) {
+		return BufferUtils.createFloatBuffer(size);
+	}
+	
+	public static FloatBuffer createFlippedBuffer(Vertex[] vertices) {
+		FloatBuffer buffer = createFloatBuffer(vertices.length * Vertex.SIZE);
+		
+		for (int i = 0; i < vertices.length; i++) {
+			buffer.put(vertices[i].getPos().getX());
+			buffer.put(vertices[i].getPos().getY());
+			buffer.put(vertices[i].getPos().getZ());
+		}
+		
+		buffer.flip();
+		
+		return buffer;
 	}
 }
